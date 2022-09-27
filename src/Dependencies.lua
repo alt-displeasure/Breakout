9480620c87e2f450d8c21614a47f43e91fbe5e28
Sofia <sofiamoramaldonado@gmail.com>
2022-09-26 21:50:55 -0300

    Util for creating quads and slicing tables

--- src/Dependencies.lua
+++ src/Dependencies.lua
@@ -1,8 +1,11 @@
 push = require 'push'
 Class = require 'class'
 
+require 'src/Util'
+
 require 'src/constants'
 require 'src/StateMachine'
 
 require 'src/states/BaseState'
 require 'src/states/StartState'
+require 'src/states/PlayState'
